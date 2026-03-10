d8b969d6765b0e2ca81c169bccc7efd7f76ee146
bolgarka38 <olejnikgeorgij475@gmail.com>
2026-03-10 21:25:03 +0300

    changed

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 #include <locale.h>
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    /* ���� ��������� ���������� */
-    printf("=== ���� ��������� ���������� ===\n");
+    printf("Matrix exponential test\n");
     matrix *A = matrix_alloc(3, 3);
     *matrix_ptr(A,0,0) = 1; *matrix_ptr(A,0,1) = 0; *matrix_ptr(A,0,2) = 0;
     *matrix_ptr(A,1,0) = 0; *matrix_ptr(A,1,1) = 2; *matrix_ptr(A,1,2) = 0;
@@ -15,18 +13,14 @@ int main() {
     matrix_print(A);
 
     matrix *expA = matrix_exp(A, 1e-10);
-    printf("exp(A) � ��������� 1e-10:\n");
+    printf("exp(A) accurate to 1e-10:\n");
     matrix_print(expA);
-    /* ������� ������������ ������� � ���������� e, e^2, 1/e */
+  
     matrix_free(expA);
     matrix_free(A);
 
-    /* ���� ������� ���� */
-    printf("\n=== ���� ������� ���� ===\n");
-    /* �������:
-     * 2x + y = 5
-     * x - y = 1
-     * �������: x=2, y=1 */
+    printf("\n Linear system test\n");
+
     matrix *A2 = matrix_alloc(2,2);
     *matrix_ptr(A2,0,0)=2; *matrix_ptr(A2,0,1)=1;
     *matrix_ptr(A2,1,0)=1; *matrix_ptr(A2,1,1)=-1;
@@ -42,20 +36,55 @@ int main() {
 
     matrix *X2 = matrix_solve(A2, B2);
     if (X2) {
-        printf("������� X =\n");
+        printf("Answer X =\n");
         matrix_print(X2);
 
-        /* ��������: �������� A*X - B � ����� ������� */
+     
         matrix *AX = matrix_alloc(1,2);
         matrix_mul2(AX, A2, X2);
         matrix_sub(AX, B2);
-        printf("���������� ||A X - B|| = %f\n", matrix_norm(AX));
+        printf("Residual ||A X - B|| = %f\n", matrix_norm(AX));
         matrix_free(AX);
         matrix_free(X2);
-    } else printf("������� ���������!\n");
+    } else printf("Singular linear system!\n");
 
     matrix_free(A2);
     matrix_free(B2);
+
+
+
+    printf("\nSecond test\n");
+    matrix *A3 = matrix_alloc(3, 3);
+    *matrix_ptr(A3,0,0) = 0; *matrix_ptr(A3,0,1) = 0; *matrix_ptr(A3,0,2) = 1;
+    *matrix_ptr(A3,1,0) = 0; *matrix_ptr(A3,1,1) = 1; *matrix_ptr(A3,1,2) = 0;
+    *matrix_ptr(A3,2,0) = 1; *matrix_ptr(A3,2,1) = 0; *matrix_ptr(A3,2,2) = 0;
+
+    matrix *B3 = matrix_alloc(1, 3);
+    *matrix_ptr(B3,0,0) = 1;
+    *matrix_ptr(B3,1,0) = 2;
+    *matrix_ptr(B3,2,0) = 3;
+
+    printf("A =\n");
+    matrix_print(A3);
+    printf("B =\n");
+    matrix_print(B3);
+
+    matrix *X3 = matrix_solve(A3, B3);
+    if (X3) {
+        printf("Answer X =\n");
+        matrix_print(X3);
+        matrix *AX3 = matrix_alloc(1, 3);
+        matrix_mul2(AX3, A3, X3);
+        matrix_sub(AX3, B3);
+        printf("Residual ||A X - B|| = %g\n", matrix_norm(AX3));
+        matrix_free(AX3);
+        matrix_free(X3);
+    } else {
+        printf("Singular linear system!\n");
+    }
+
+    matrix_free(A3);
+    matrix_free(B3);
 
     return 0;
 }
